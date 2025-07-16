a5688442c6368b3714127ed0e1cb6626a80feaff
Alemichitakittyuwu99 <alemyco@gmail.com>
2025-07-16 16:05:25 -0500

    Firts Commit

---
+++ database/migrations/2025_07_16_000001_create_usuario_table.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('usuario', function (Blueprint $table) {
+            $table->id('id_usuario');
+            $table->string('nombre');
+            $table->string('apellido');
+            $table->date('fecha_nacimiento');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('usuario');
+    }
+};
